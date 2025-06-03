6d7eb2800431ce214cb23cdac965bede0892a03a
Nalinka Heshan <Nalinka.Heshan@cloudsolutions.com.sa>
2025-06-03 23:32:10 +0530

    Initial Commit

---
+++ frontend/task-manager/src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const loginData = this.loginForm.value;
+    console.log('Login payload:', loginData);
+
+  }
+}
